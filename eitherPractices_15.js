637c403faf0b254f617bb5e608149b02d048a240
Tidalu <jsjsdq65@gmail.com>
2023-06-17 13:34:07 +0200

    either monad. and practices

---
+++ eitherPractices_15.js
@@ -0,0 +1,118 @@
+// Definitions
+// ====================
+const Right = x =>
+({
+    chain: f => f(x),
+    map: f => Right(f(x)),
+    fold: (f, g) => g(x),
+    toString: () => `Right(${x})`
+})
+
+const Left = x =>
+({
+    chain: f => Left(x),
+    map: f => Left(x),
+    fold: (f, g) => f(x),
+    toString: () => `Left(${x})`
+})
+
+const fromNullable = x =>
+    x != null ? Right(x) : Left(null)
+
+const tryCatch = f => {
+    try {
+        return Right(f())
+    } catch (e) {
+        return Left(e)
+    }
+}
+
+const logIt = x => {
+    console.log(x)
+    return x
+}
+
+const DB_REGEX = /postgres:\/\/([^:]+):([^@]+)@.*?\/(.+)$/i
+
+// Exercise: Either
+// Goal: Refactor each example using Either
+// Bonus: no curlies
+// =========================
+
+
+// Ex1: Refactor streetName to use Either instead of nested if's
+// =========================
+const street = user =>
+    fromNullable(user.address) // right /left(address)
+        .map(address => address.street)
+        .fold(() => 'no street', x => x)
+
+
+
+QUnit.test("Ex1: street", assert => {
+    const user = { address: { street: { name: "Willow" } } }
+    assert.deepEqual(street(user), { name: "Willow" })
+    assert.equal(street({}), "no street")
+})
+
+// Ex1: Refactor streetName to use Either instead of nested if's
+// =========================
+const streetName = user => {
+    return fromNullable(user)
+        .chain(user => fromNullable(user.address))
+        .chain(address => fromNullable(address.street))
+        .map(street => street.name)
+        .fold(() => 'no street', x => x)
+
+}
+
+QUnit.test("Ex1: streetName", assert => {
+    const user = { address: { street: { name: "Willow" } } }
+    assert.equal(streetName(user), "Willow")
+    assert.equal(streetName({}), "no street")
+    assert.equal(streetName({ address: { street: null } }), "no street")
+})
+
+
+// Ex2: Refactor parseDbUrl to return an Either instead of try/catch
+// =========================
+const parseDbUrl = cfg => {
+    try {
+        const c = JSON.parse(cfg) // throws if it can't parse
+        return c.url.match(DB_REGEX)
+    } catch (e) {
+        return null
+    }
+}
+
+QUnit.test("Ex1: parseDbUrl", assert => {
+    const config = '{"url": "postgres://sally:muppets@localhost:5432/mydb"}'
+    assert.equal(parseDbUrl(config)[1], "sally")
+    assert.equal(parseDbUrl(), null)
+})
+
+
+
+// Ex3: Using Either and the functions above, refactor startApp
+// =========================
+const startApp = cfg => {
+    const parsed = parseDbUrl(cfg)
+
+    if (parsed) {
+        const [_, user, password, db] = parsed
+        return `starting ${db}, ${user}, ${password}`
+    } else {
+        return "can't get config"
+    }
+}
+
+
+
+
+QUnit.test("Ex3: startApp", assert => {
+    const config = '{"url": "postgres://sally:muppets@localhost:5432/mydb"}'
+    assert.equal(String(startApp(config)), "starting mydb, sally, muppets")
+    assert.equal(String(startApp()), "can't get config")
+})
+
+// go here https://codepen.io/Tidalu/pen/RwqaLpV?editors=0010 
--- refactoringUsingEitherMonad_14.js
+++ refactoringUsingEitherMonad_14.js
@@ -1,1 +1,64 @@
 // holo
+// Definitions
+
+
+import fs from 'fs'
+// ====================
+const Right = x =>
+({
+    chain: f => f(x),
+    map: f => Right(f(x)),
+    fold: (f, g) => g(x),
+    inspect: `Right(${x})`
+})
+
+const Left = x =>
+({
+    chain: f => Left(x),
+    map: f => Left(x),
+    fold: (f, g) => f(x),
+    inspect: `Left(${x})`
+
+
+})
+
+const fromNullable = x =>
+    x != null ? Right(x) : Left(null)
+
+const tryCatch = f => {
+    try {
+        return Right(f())
+    }catch(e){
+        return Left(e)
+    }
+}
+
+const getPort_ = () => {
+    try {
+        const str = fs.readFileSync('config.json')
+        const config = JSON.parse(str)
+        return config.port
+    } catch(e) {
+        return 54000
+    }
+}
+const readFileSync = path => 
+    tryCatch(() => fs.readFileSync(path))
+const getPort__ = () => 
+    readFileSync('config.json')
+    .map( content => JSON.parse(content))
+    .map(config => config.port)
+    .fold(() => 8080, x => x)
+
+    const parseJSON = contents => 
+        tryCatch(() => JSON.parse(contents))
+    
+    // flattering either monad with chain
+    const getPort = () => 
+        readFileSync('config.json')
+        .chain( content => parseJSON(content))
+        .map(config => config.port)
+        .fold(() => 8080, x => x)
+
+const res = getPort()
+console.log(res)
